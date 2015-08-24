166f698316862b857f9be33783d5d150977c4f30
DollarAkshay <akshay95aradhya@gmail.com>
2015-08-24 18:29:09 +0530

    Solved Power of Thor in Python :snake:

---
+++ 1. Easy/Python/Power_of_Thor.py
@@ -0,0 +1,22 @@
+import sys
+import math
+
+lx, ly, tx, ty = [int(i) for i in raw_input().split()]
+
+while 1:
+    remainingTurns = int(raw_input())
+    dir = ""
+    if ly<ty:
+        dir+='N'
+        ty-=1
+    elif ly>ty:
+        dir+='S'
+        ty+=1
+        
+    if lx<tx:
+        dir+='W'
+        tx-=1
+    elif lx>tx:
+        dir+='E'
+        tx+=1
+    print dir
