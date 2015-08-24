942bcaf35f5ac749dd42d34dcb13e4bc5adddb9d
DollarAkshay <akshay95aradhya@gmail.com>
2015-08-24 18:55:16 +0530

    Solved The_Descent in Python

---
+++ 1. Easy/Python 3/The_Descent.py
@@ -0,0 +1,7 @@
+﻿while 1:
+    sX, sY = [int(i) for i in input().split()]
+    m = [int(input()) for i in range(8)]
+    if m.index(max(m)) == sX:
+        print ("FIRE")
+    else :
+        print ("HOLD")
