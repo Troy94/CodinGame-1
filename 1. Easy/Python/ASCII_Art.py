a41d412363e4cd0d01387dd0349b7f89bd5177f3
DollarAkshay <akshay95aradhya@gmail.com>
2015-08-24 20:40:08 +0530

    Solved ASCII Art in Python :snake:

---
+++ 1. Easy/Python/ASCII_Art.py
@@ -0,0 +1,20 @@
+﻿import sys
+import math
+
+L = int(raw_input())
+H = int(raw_input())
+T = list(raw_input())
+for i in range(len(T)):
+    if T[i].isalpha():
+        T[i] = T[i].upper()
+    else:
+        T[i] = '['
+T = ''.join(T)
+set = [ raw_input() for i in range(H)]
+ans=""
+for i in range(H):
+    for c in T:
+        n = (ord(c) - ord('A'))*L
+        ans+=set[i][n:n+L]
+    ans+="\n"
+print ans
