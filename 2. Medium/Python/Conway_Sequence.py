61afc3608736bc94940b904136c757e261d98c9d
DollarAkshay <akshay95aradhya@gmail.com>
2015-08-25 23:56:30 +0530

    Solved Conway's Sequence in Python :snake:

---
+++ 2. Medium/Python/Conway_Sequence.py
@@ -0,0 +1,19 @@
+﻿a = [int(raw_input())]
+it = int(raw_input())
+
+for i in range(it-1):
+    f = 0;
+    c = a[0]
+    b = []
+    for x in a:
+        if x==c:
+            f+=1
+        else:
+            b.extend([f,c])
+            f = 1
+            c = x
+    b.extend([f,c])
+    a = b
+    
+print ' '.join(map(str, a))
+            
