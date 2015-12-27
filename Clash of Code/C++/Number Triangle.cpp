cf6cee2e47455b1d597914ab87a7e45fa97084ba
DollarAkshay <akshay95aradhya@gmail.com>
2015-12-27 07:15:47 +0530

    Adding a few Clash of Code Problems

---
+++ Clash of Code/C++/Number Triangle.cpp
@@ -0,0 +1,58 @@
+/*~~~~~~~~~~~~~~~~~~*
+ *                  *
+ * $Dollar Akshay$  *
+ *                  *
+ *~~~~~~~~~~~~~~~~~~*/
+
+//
+
+#include <math.h>
+#include <time.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define sp system("pause")
+#define FOR(i,a,b) for(int i=a;i<=b;++i)
+#define FORD(i,a,b) for(int i=a;i>=b;--i)
+#define REP(i,n) FOR(i,0,(int)n-1)
+#define pb(x) push_back(x)
+#define mp(a,b) make_pair(a,b)
+#define DB(format,...) fprintf(stderr,format, ##__VA_ARGS__)
+#define MS(x,n) memset(x,n,sizeof(x))
+#define SORT(a,n) sort(begin(a),begin(a)+n)
+#define REV(a,n) reverse(begin(a),begin(a)+n)
+#define ll long long
+#define pii pair<int,int>
+#define MOD 1000000007
+
+int a[1000][1000];
+int sum[100];
+
+int main(){
+
+
+	int t;
+	scanf("%d", &t);
+	REP(i, t) {
+		REP(j, t-i)
+			printf("%d", t);
+		printf("\n");
+	}
+	return 0;
+}
+
+//
