3fdd0650801f209cb0d8e787b51f38e1cce95042
DollarAkshay <akshay95aradhya@gmail.com>
2018-04-23 03:43:43 +0530

    Solving a community problem in JS

---
+++ Community Puzzles/Javascript/The_Greatest_Number.js
@@ -0,0 +1,19 @@
+var n = parseInt(readline());
+var chars = readline().split(' ')
+chars.sort();
+
+if(chars.includes('-')){
+    if(chars.includes('.')) {
+        chars.splice(1, 1);
+        chars.splice(2, 0, '.');
+    }
+}
+else{
+    chars.reverse();
+    if(chars.includes('.')) {
+        chars.splice(-1, 1);
+        chars.splice(-1, 0, '.');
+    }
+}
+
+print(parseFloat(chars.join('')));
