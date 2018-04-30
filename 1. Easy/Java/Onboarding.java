e1e6c8cd7a4bd16e3191a3aad788707576190db9
DollarAkshay <akshay95aradhya@gmail.com>
2018-05-01 04:45:01 +0530

    Code Cleanup :shower:

--- 1. Easy/Java/Onboarding.java
+++ 1. Easy/Java/Onboarding.java
@@ -16,10 +16,10 @@ class Player {
         Scanner in = new Scanner(System.in);
 
         while (true) {
-            String enemy1 = in.next(); // name of enemy 1
-            int dist1 = in.nextInt(); // distance to enemy 1
-            String enemy2 = in.next(); // name of enemy 2
-            int dist2 = in.nextInt(); // distance to enemy 2
+            String enemy1 = in.next();
+            int dist1 = in.nextInt();
+            String enemy2 = in.next();
+            int dist2 = in.nextInt();
 
             if (dist1 < dist2) {
                 System.out.println(enemy1);
