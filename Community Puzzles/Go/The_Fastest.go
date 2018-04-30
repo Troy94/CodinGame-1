1211c334374a2269a40bfbfe293eb6ac9c266437
DollarAkshay <akshay95aradhya@gmail.com>
2018-05-01 04:12:43 +0530

    First ever go program :grin:

---
+++ Community Puzzles/Go/The_Fastest.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+import "sort"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	var a []string
+
+	for i := 0; i < n; i++ {
+		var t string
+		fmt.Scan(&t)
+		a = append(a, t)
+	}
+
+	sort.Strings(a)
+	fmt.Println(a[0])
+}
